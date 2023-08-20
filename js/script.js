30f1b548d4e154f290ae435f0b69f3233587e458
zijaigidov <igidov.zija@gmail.com>
2023-08-20 18:03:45 +0200

    Change Rock Paper Scissors move names to lowercase
    Changed the names of the moves to lowercase, for example replacing
    'Rock' with 'rock' so that case insensitivity can be implemented
    easier using .toLowerCase()

--- js/script.js
+++ js/script.js
@@ -13,6 +13,6 @@ function showGameInfo() {
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3) + 1; // Random integer in the interval 1-3
   const computerChoice =
-    randomNumber === 1 ? 'Rock' : randomNumber === 2 ? 'Paper' : 'Scissors';
+    randomNumber === 1 ? 'rock' : randomNumber === 2 ? 'paper' : 'scissors';
   return computerChoice;
 }
