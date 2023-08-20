6136c9f5bfff0605fd8307657041f92adfc226b9
zijaigidov <igidov.zija@gmail.com>
2023-08-20 18:14:47 +0200

    Add function for playing a round
    - NOTE: The function doesn't take input validation into account

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ function getComputerChoice() {
     randomNumber === 1 ? 'rock' : randomNumber === 2 ? 'paper' : 'scissors';
   return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+  switch (playerSelection) {
+    case computerSelection:
+      return `It's a draw! Both players chose ${playerSelection}`;
+    case 'rock':
+      return computerSelection === 'paper'
+        ? 'You lose! Paper beats rock'
+        : 'You win! Rock beats scissors';
+    case 'paper':
+      return computerSelection === 'rock'
+        ? 'You win! Paper beats rock'
+        : 'You lose! Scissors beats paper';
+    case 'scissors':
+      return computerSelection === 'rock'
+        ? 'You lose! Rock beats scissors'
+        : 'You win! Scissors beats paper';
+  }
+}
